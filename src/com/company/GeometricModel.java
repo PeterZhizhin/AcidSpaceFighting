f7d376cc4f64b568d9b600fd95432a96b12a0fb9
ShirinkinArseny <a-arsenij@yandex.ru>
2014-01-18 00:23:45 +0600

    Первый коммит дэсу.

---
+++ src/com/company/GeometricModel.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.awt.*;
+
+public class GeometricModel {
+
+       private Point[] vetrexes;
+
+    public GeometricModel(Point[] vrtx) {
+        vetrexes=vrtx;
+    }
+
+
+
+
+}
