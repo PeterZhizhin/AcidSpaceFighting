40e72f037c0f1b421207f2871cd562647de61883
ShirinkinArseny <a-arsenij@yandex.ru>
2014-01-18 00:49:56 +0600

    Основы геометрии.

--- src/com/company/GeometricModel.java
+++ src/com/company/GeometricModel.java
@@ -1,13 +1,48 @@
 package com.company;
 
-import java.awt.*;
-
 public class GeometricModel {
 
-       private Point[] vetrexes;
+    private Point center;
+    private Point[] vertexes;
+    private Point[] sourceVertexes;
+    private float angle;
 
-    public GeometricModel(Point[] vrtx) {
-        vetrexes=vrtx;
+    public void rotate(float angle) {
+        this.angle+=angle;
+        for (int i=0; i<vertexes.length; i++) {
+            vertexes[i].set(sourceVertexes[i]);
+            vertexes[i].rotate(angle);
+            vertexes[i].move(center);
+        }
+    }
+
+    public void move (Point p) {
+       center.move(p);
+        for (Point vertex : vertexes) {
+            vertex.move(p);
+        }
+    }
+
+    public boolean contains(Point p) {
+        return false;
+        //check
+    }
+
+    public boolean intersect(GeometricModel model) {
+        return false;
+        //check
+    }
+
+    public Point getPoint(int pointNum) {
+        return vertexes[pointNum];
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public GeometricModel() {
+
     }
 
 
---
+++ src/com/company/Point.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Point {
+
+    private float x, y;
+
+    public void rotate(float angle) {
+        double newX=x*Math.cos(angle)-y*Math.sin(angle);
+        double newY=x*Math.sin(angle)+y*Math.cos(angle);
+        x= (float) newX;
+        y= (float) newY;
+    }
+
+    public void move(float dx, float dy) {
+        x+=dx;
+        y+=dy;
+    }
+
+    public void move(Point p) {
+        x+=p.x;
+        y+=p.y;
+    }
+
+    public void set(float x, float y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    public void set(Point p) {
+        set(p.x, p.y);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public Point(float x, float y) {
+        this.x=x;
+        this.y=y;
+    }
+
+
+}
