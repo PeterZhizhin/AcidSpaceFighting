56bce3239ed16abc2462ab9de778d415ea3a273b
ShirinkinArseny <a-arsenij@yandex.ru>
2014-01-18 00:21:23 +0600

    Первый коммит дэсу.

---
+++ src/com/company/Window.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Window {
+
+    private static boolean isWorking = true;
+
+    private static void initDisplay() {
+        try {
+            int w = 1000;
+            int h = 700;
+            Display.setDisplayMode(new DisplayMode(w, h));
+            Display.setVSyncEnabled(true);
+            Display.create();
+            glClearColor(0f, 0f, 0f, 1f);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glLineWidth(2f);
+            glOrtho(0, w, h, 0, 1, 0);
+            Mouse.create();
+        } catch (LWJGLException e) {
+            System.err.println("Failed to init GL window");
+            exit();
+        }
+    }
+
+    private static void initUpdateThread() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (isWorking)
+                {
+
+                }
+                else cancel();
+            }
+        }, 0, 20);
+    }
+
+    private static void drawWhileWorks() {
+        while (!Display.isCloseRequested()) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            //draw
+            Display.sync(60);
+            Display.update();
+        }
+    }
+
+    private static void exit() {
+        isWorking = false;
+        Mouse.destroy();
+        Display.destroy();
+        System.out.println("Exiting");
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        initDisplay();
+        initUpdateThread();
+        drawWhileWorks();
+        exit();
+    }
+}
