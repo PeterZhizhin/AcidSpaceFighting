e46d2d5d76d429f58c7ec19bda4b0b7225651c18
PeterZhizhin <piter.z@bk.ru>
2014-01-27 21:20:07 +0600

    Back to old

--- src/com/company/World.java
+++ src/com/company/World.java
@@ -6,8 +6,11 @@ import com.company.Graphic.Camera;
 import com.company.Graphic.GraphicModel;
 import com.company.Models.Asteroid.AsteroidGeometricModel;
 import com.company.Models.Asteroid.AsteroidGraphicModel;
+import com.company.Models.RocketBase.RocketBaseGeometricModel;
+import com.company.Models.RocketBase.RocketBaseGraphicModel;
 import com.company.Physic.PhysicModel;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -26,20 +29,23 @@ public class World {
     }
 
     public static void draw() {
+        Camera.setPosition(rocket.getCentre().getX(), rocket.getCentre().getY());
         for (Model model : models) {
             model.draw();
         }
     }
 
     public static void update(float deltaTime) {
-         for (int i=0; i<models.size()-1; i++)
-             for (int j=i+1; j<models.size(); j++) {
-                 models.get(i).crossThem(models.get(j), deltaTime);
-             }
+        for (int i=0; i<models.size()-1; i++)
+            for (int j=i+1; j<models.size(); j++) {
+                models.get(i).crossThem(models.get(j), deltaTime);
+            }
         for (Model model : models) {
             model.updateMotion(deltaTime);
         }
     }
+
+    private static GeometricModel rocket;
 
     public static void init() {
         models=new ArrayList<Model>();
@@ -47,15 +53,21 @@ public class World {
         Random rnd=new Random();
         for (int i=0; i<2; i++)
             for (int j=0; j<2; j++) {
-        float width=rnd.nextInt(200)+200;
-        float distance=2000f;
+                float width=rnd.nextInt(600)+600;
+                float distance=4000f;
 
 
-        GeometricModel g=new AsteroidGeometricModel(i*distance, j*distance, width);
-        PhysicModel p = new PhysicModel(g, width);
-        Model m=new Model(new AsteroidGraphicModel(g), p);
-        models.add(m);
+                GeometricModel g=new AsteroidGeometricModel(i*distance, j*distance, width);
+                PhysicModel p = new PhysicModel(g, width);
+                Model m=new Model(new AsteroidGraphicModel(g), p);
+                models.add(m);
             }
+
+        GeometricModel g=new RocketBaseGeometricModel(500, 500, 250f);
+        rocket=g;
+        PhysicModel e=new PhysicModel(g, 15f);
+        Model m=new Model(new RocketBaseGraphicModel(g), e);
+        models.add(m);
     }
 
 }
