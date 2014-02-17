f554e100c171cf27fde11cf585f348901603634a
PtPepper <Kro.yegor@mail.ru>
2014-02-17 18:11:11 +0600

    Textures

--- src/com/company/Models/Base/BaseGraphicModel.java
+++ src/com/company/Models/Base/BaseGraphicModel.java
@@ -7,7 +7,7 @@ import com.company.Graphic.TextureDrawer;
 public class BaseGraphicModel extends GraphicModel {
 
     public void drawTopLayer() {
-        TextureDrawer.drawQuad(shape.getPoint(0), shape.getPoint(1), shape.getPoint(2), shape.getPoint(3), 0);
+        TextureDrawer.drawQuad(shape.getPoint(0),  shape.getPoint(1), shape.getPoint(2), shape.getPoint(3), 0);
     }
 
     public BaseGraphicModel(GeometricModel body) {
